6ecff062f3a6d3720cdac04c0f2302dda00563f5
yeeahG <yeji.sep.k@gmail.com>
2022-06-09 00:44:20 +0900

    BrowserRouter 생성

---
+++ tomyanimal/src/App.js
@@ -0,0 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <BrowserRouter>
+        
+        </BrowserRouter>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
