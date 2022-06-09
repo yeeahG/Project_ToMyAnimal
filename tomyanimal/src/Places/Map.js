8538430d97866e36ac2452bde33fcaa6e5235b74
yeeahG <yeji.sep.k@gmail.com>
2022-06-10 02:13:19 +0900

    Kakap map 로딩을 위해 api key 설정, .env file setting, 화면에map loading까지

--- tomyanimal/src/Places/Map.js
+++ tomyanimal/src/Places/Map.js
@@ -1,8 +1,23 @@
-import React from 'react'
+/*global kakao */ 
+import React, { useEffect } from 'react'
 
 const Map = () => {
+  useEffect(() => {
+    let container = document.getElementById("map");
+
+    let options = {
+      center: new window.kakao.maps.LatLng(35.85133, 127.734086),
+      level: 9,
+    };
+
+    let map = new window.kakao.maps.Map(container, options);
+
+    console.log("loading kakaomap");
+  }, []);
+
   return (
     <div>
+        <div id="map" style={{width:'500px', height:"400px"}}></div>
         Map
     </div>
   )
