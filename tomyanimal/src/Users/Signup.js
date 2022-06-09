64a02b0d1a821d00d1359a9f1598ff0434e02f91
yeeahG <yeji.sep.k@gmail.com>
2022-06-09 10:10:20 +0900

    signup css

--- tomyanimal/src/Users/Signup.js
+++ tomyanimal/src/Users/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import './Signup.css'
 
 const Signup = () => {
     let [phoneNumber, setPhoneNumber] = useState("");
