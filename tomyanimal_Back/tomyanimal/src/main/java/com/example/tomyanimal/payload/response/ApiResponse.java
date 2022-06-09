291cd2914c1a8b1b807b0a8a515be9dd15cb1157
JSL107 <juneseok0107@naver.com>
2022-06-09 23:57:53 +0900

    Response들 구현

---
+++ tomyanimal_Back/tomyanimal/src/main/java/com/example/tomyanimal/payload/response/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.example.tomyanimal.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private Boolean success;
+    private String message;
+
+}
