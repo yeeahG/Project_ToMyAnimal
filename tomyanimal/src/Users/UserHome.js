010de1c6a949eb8aaf12b2e2d7d9761aaca5e465
yeeahG <yeji.sep.k@gmail.com>
2022-06-09 10:11:50 +0900

    User page에 css 적용

--- tomyanimal/src/Users/UserHome.js
+++ tomyanimal/src/Users/UserHome.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import './UserHome.css'
 
 const UserHome = () => {
     const navigate = useNavigate();
--- tomyanimal/src/Users/UserLogin.js
+++ tomyanimal/src/Users/UserLogin.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import './UserLogin.css'
 
 const UserLogin = () => {
     const [details, setDetails] = useState({id:"", password: ""});
