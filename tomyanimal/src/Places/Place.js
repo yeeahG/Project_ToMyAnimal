eac8bea8d4b879d826579d935a5c5803024a5962
yeeahG <yeji.sep.k@gmail.com>
2022-06-10 01:07:24 +0900

    Places content 작성

--- tomyanimal/src/Places/Place.js
+++ tomyanimal/src/Places/Place.js
@@ -22,7 +22,57 @@ const Place = () => {
         Map
       </div>
 
-      
+      <div className='place__content'>
+
+        <div className='left__menu'>
+          <ul className='menu__wrap'>
+            <li className='menu__list'>지역들</li>
+            <div className='collection'>
+              <a className='collection__content'>전체</a>
+            </div>
+            <div className='collection'>
+              <a className='collection__content' href=''>Seoul</a>
+            </div>
+            <div className='collection'>
+              <a className='collection__content'>Daegu</a>
+            </div>
+          </ul>
+        </div>
+
+        <div className='content__wrapper'>
+          <div className='animal__imageform'>
+            List
+          </div>
+
+          <div className='info__details'>
+
+            <div className='details__description'>
+              <h1>I'm choco</h1>
+              <p>2years old</p>
+              <p>Lorem</p>
+            </div>
+
+            <div className='stack'>
+              <button className='details__btn' aria-expanded="false">
+                <span>Log</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="14.255" height="14.255">
+                  <path fill='none' stroke="currentcolor" strokeWidth="1.5" d="M7.129 0v14.255M0 7.129h14.255"></path>
+                </svg>
+              </button>
+              <div className='details__panel'>
+                <p>세부내용 aria-expanded="true"로 변경되면서 이 부분이 열림
+                  d="M0 7.128h14.255"로 바꿔주기
+                </p>
+              </div>
+            </div>
+            
+          </div>
+
+        </div>
+
+      </div>
+
+
     </div>
   )
 }
