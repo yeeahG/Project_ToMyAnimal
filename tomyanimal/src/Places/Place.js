c0ae3b89ad783109eb57fc445bb426394f6f8ffd
yeeahG <yeji.sep.k@gmail.com>
2022-06-10 01:03:23 +0900

    Map container 작성

--- tomyanimal/src/Places/Place.js
+++ tomyanimal/src/Places/Place.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Place = () => {
   return (
-    <div>Place</div>
+    <div className='place__container'>
+
+      <div className='header'>
+        <div className='space'></div>
+        <div className='header__wrapper'>
+          <h1 className='header__content'>Header</h1>
+          <div className='header__detail'>
+            <p>details</p>
+          </div>
+        </div>
+      </div>
+        
+      <div className='space'></div>
+      <div className='line'></div>
+      <div className='space'></div>
+        
+      <div className='map__container'>
+        Map
+      </div>
+
+      
+    </div>
   )
 }
 
