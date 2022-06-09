9ad0ab80e7de323aef1e15d4e0583292c980d866
JSL107 <juneseok0107@naver.com>
2022-06-09 23:47:30 +0900

    Security Config 정의

--- tomyanimal_Back/tomyanimal/src/main/java/com/example/tomyanimal/config/SecurityConfig.java
+++ tomyanimal_Back/tomyanimal/src/main/java/com/example/tomyanimal/config/SecurityConfig.java
@@ -1,9 +1,22 @@
 package com.example.tomyanimal.config;
 
+import com.example.tomyanimal.security.CustomUserDetailsService;
+import com.example.tomyanimal.security.JwtAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 // 웹 보안 가능
@@ -11,4 +24,42 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 // 메소드 보안
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    // JwtAuthenticationEntryPoint : 인증 절차 없이 자원에 엑세스 하면 클라이언트에게 401 오류 반환
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+//    사용자 인증을 위한 Spring Security를 생성하는데 사용
+//    메모리 내 인증, JDBC인증, 사용자 정의 등을 사용할수 있음
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+//    HttpSecurity 구성과 같은 보안 기능을 구성하는데 사용
+//    csrf, sessionManagement등의 보호 기능 및 규칙을 추가 할 수 있음
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+                .antMatchers("/", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg", "/**/*.html",
+                        "/**/*.css", "/**/*.js")
+                .permitAll().antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**").permitAll().anyRequest().authenticated();
+
+    }
 }
