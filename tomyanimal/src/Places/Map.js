ff9baa97ba5c7945f0723043e1398907814f2fd3
yeeahG <yeji.sep.k@gmail.com>
2022-06-10 02:26:19 +0900

    marker data 설정 준비

--- tomyanimal/src/Places/Map.js
+++ tomyanimal/src/Places/Map.js
@@ -15,6 +15,21 @@ const Map = () => {
     const map = new window.kakao.maps.Map(container, options);
 
     console.log("loading kakaomap");
+
+    // //marker data 파일에서 가져옴
+    // markerdata.forEach((el) => {
+    // //마커를 생성
+    //     const marker = new kakao.maps.Marker({
+    //     //마커가 표시 될 지도
+    //     map: map,
+    //     //마커가 표시 될 위치
+    //     position: new kakao.maps.LatLng(el.lat, el.lng),
+    //     });
+    //     // 마커에 표시할 인포윈도우를 생성합니다
+    //     var infowindow = new kakao.maps.InfoWindow({
+    //         content: el.price, // 인포윈도우에 표시할 내용
+    //     });
+    // })
   }, []);
 
   return (
