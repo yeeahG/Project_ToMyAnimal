caae3349801cba3a1d16bc8e595414b2ed8aa815
yeeahG <yeji.sep.k@gmail.com>
2022-06-10 17:06:37 +0900

    data file에 있는 시설목록들 지도에 표시

--- tomyanimal/src/Places/Map.js
+++ tomyanimal/src/Places/Map.js
@@ -1,5 +1,6 @@
 /*global kakao */ 
 import React, { useEffect } from 'react'
+import { placeData } from './placeData'
 import './Map.css'
 
 const Map = () => {
@@ -9,7 +10,7 @@ const Map = () => {
 
     const options = {
       center: new window.kakao.maps.LatLng(35.85133, 127.734086),
-      level: 9,
+      level: 11,
     };
 
     const map = new window.kakao.maps.Map(container, options);
@@ -32,33 +33,37 @@ const Map = () => {
     // })
 
 
-    
+
     // 주소-좌표 변환 객체를 생성합니다
   var geocoder = new kakao.maps.services.Geocoder();
 
-  // 주소로 좌표를 검색합니다
-  geocoder.addressSearch('제주특별자치도 제주시 첨단로 242', function(result, status) {
-
-    // 정상적으로 검색이 완료됐으면 
-    if (status === kakao.maps.services.Status.OK) {
-
-      var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-      // 결과값으로 받은 위치를 마커로 표시합니다
-      var marker = new kakao.maps.Marker({
-        map: map,
-        position: coords
-      });
-
-      // 인포윈도우로 장소에 대한 설명을 표시합니다
-      var infowindow = new kakao.maps.InfoWindow({
-        content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
-      });
-      infowindow.open(map, marker);
-
-      // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-      map.setCenter(coords);
-    }
+  //foreach loop
+  placeData.forEach((el) => {
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(el.addr, function(result, status) {
+    
+      // 정상적으로 검색이 완료됐으면 
+      if (status === kakao.maps.services.Status.OK) {
+    
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+    
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        var marker = new kakao.maps.Marker({
+          map: map,
+          position: coords
+        });
+    
+        // 인포윈도우로 장소에 대한 설명을 표시합니다
+        var infowindow = new kakao.maps.InfoWindow({
+          content: '<div style="width:150px;text-align:center;padding:6px 0;">title</div>'
+        });
+        infowindow.open(map, marker);
+    
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+        }
+    })
+      
   });
 
 
--- tomyanimal/src/Places/placeData.js
+++ tomyanimal/src/Places/placeData.js
@@ -1,13 +1,13 @@
-export const markerdata = [
+export const placeData = [
     {
       title: "드린겐애견테마파크",
-      add: "미산동 163-1 KR",
+      addr: "미산동 163-1 KR",
       lat: 33.2475582,
       lng: 126.4081161,
     },
     {
       title: "반려견 교육 기관 위드",
-      add: "고봉동 지영로 257-17",
+      addr: "고봉동 지영로 257-17",
       lat: 33.2460024,
       lng: 126.5810125,
     }
