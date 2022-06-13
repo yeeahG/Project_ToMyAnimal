e46eeabc3187c2bc666e4209a62d27f988f894f0
JSL107 <juneseok0107@naver.com>
2022-06-13 21:42:16 +0900

    localhost:3000 í 회원가입

--- Back_toMyAnimal/src/main/java/team1/toMyAnimal/controller/response/Response.java
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/controller/response/Response.java
@@ -5,23 +5,23 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@JsonInclude(JsonInclude.Include.NON_NULL) // 1
-@AllArgsConstructor(access = AccessLevel.PRIVATE) // 2
-@Getter // 3
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class Response {
     private boolean success;
     private int code;
     private Result result;
 
-    public static Response success() { // 4
+    public static Response success() {
         return new Response(true, 0, null);
     }
 
-    public static <T> Response success(T data) { // 5
+    public static <T> Response success(T data) {
         return new Response(true, 0, new Success<>(data));
     }
 
-    public static Response failure(int code, String msg) { // 6
+    public static Response failure(int code, String msg) {
         return new Response(false, code, new Failure(msg));
     }
 }
--- Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/dto/request/SignInRequest.java
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/dto/request/SignInRequest.java
@@ -2,10 +2,17 @@ package team1.toMyAnimal.domain.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignInRequest {
+    
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String userId;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String userPassword;
 }
