837379f177ea26390ee6e2190ece662369d788a0
JSL107 <juneseok0107@naver.com>
2022-06-13 16:01:39 +0900

    다른 방법 BackEnd Server

---
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/ToMyAnimalApplication.java
@@ -0,0 +1,13 @@
+package team1.toMyAnimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToMyAnimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToMyAnimalApplication.class, args);
+	}
+
+}
