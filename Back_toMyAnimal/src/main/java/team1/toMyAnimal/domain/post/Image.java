db538d95a7e1cd493829200d94bb8bbd1b9974c6
JSL107 <juneseok0107@naver.com>
2022-06-16 17:27:02 +0900

    Post Entity, Image Entity 생성

---
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/post/Image.java
@@ -0,0 +1,4 @@
+package team1.toMyAnimal.domain.post;
+
+public class Image {
+}
---
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/post/Post.java
@@ -0,0 +1,62 @@
+package team1.toMyAnimal.domain.post;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import team1.toMyAnimal.domain.category.Category;
+import team1.toMyAnimal.domain.common.EntityDate;
+import team1.toMyAnimal.domain.member.Member;
+
+import javax.persistence.*;
+import javax.swing.text.StringContent;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends EntityDate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    @Lob
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Category category;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<Image> images;
+
+    public Post(String title, String content, Member member, Category category, List<Image> images){
+        this.title = title;
+        this.content = content;
+        this.member = member;
+        this.category = category;
+        this.images = new ArrayList<>();
+        addImages(images);
+
+    }
+
+    private void addImages(List<Image> added) {
+        added.stream().forEach( i -> {
+            images.add(i);
+            i.initPost(this);
+        });
+    }
+}
