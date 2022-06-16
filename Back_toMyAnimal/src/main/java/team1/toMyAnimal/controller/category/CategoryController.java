a85098fb136ff4f7b3d64cfa40a933610060cdf0
JSL107 <juneseok0107@naver.com>
2022-06-16 14:49:38 +0900

    category 컨트롤러 생성

---
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/controller/category/CategoryController.java
@@ -0,0 +1,37 @@
+package team1.toMyAnimal.controller.category;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import team1.toMyAnimal.controller.response.Response;
+import team1.toMyAnimal.domain.dto.category.CategoryCreateRequest;
+import team1.toMyAnimal.service.category.CategoryService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/api/categories")
+    @ResponseStatus(HttpStatus.OK)
+    public Response readAll() {
+        return Response.success(categoryService.readAll());
+    }
+
+    @PostMapping("/api/categories")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Response create(@Valid @RequestBody CategoryCreateRequest req) {
+        categoryService.create(req);
+        return Response.success();
+    }
+
+    @DeleteMapping("/api/categories/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Response delete(@PathVariable Long id) {
+        categoryService.delete(id);
+        return Response.success();
+    }
+}
