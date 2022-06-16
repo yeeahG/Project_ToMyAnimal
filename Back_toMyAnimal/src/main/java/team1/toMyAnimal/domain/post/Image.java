9b7bba768a66a95bd1722f3255c5170fdfad1193
JSL107 <juneseok0107@naver.com>
2022-06-16 17:36:07 +0900

    Image Entity 작성

--- Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/post/Image.java
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/post/Image.java
@@ -1,4 +1,57 @@
 package team1.toMyAnimal.domain.post;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.Arrays;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String uniqueName;
+
+    @Column(nullable = false)
+    private String originName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Post post;
+
+    private final static String supportedExtension[] = {"jpg", "jpeg", "gif", "bmp", "png"};
+
+    public image(String originName) {
+        this.uniqueName = generateUniqueName(extractExtension(originName));
+        this.originName = originName;
+    }
+
+    public void initPost(Post post) {
+        if (this.post == null) {
+            this.post = post;
+        }
+    }
+
+    private String extractExtension(String originName) {
+        try {
+            String ext = originName.substring(originName.lastIndexOf(".") + 1);
+            if ((isSupportedFormat(ext)) return ext){
+            }
+        }catch (StringIndexOutOfBoundsException e) {}
+        throw new UnsupportedImageFormatException();
+    }
+
+    private boolean isSupportedFormat(String ext) {
+        return Arrays.stream(supportedExtension).anyMatch(e -> e.equalsIgnoreCase(ext));
+    }
 }
