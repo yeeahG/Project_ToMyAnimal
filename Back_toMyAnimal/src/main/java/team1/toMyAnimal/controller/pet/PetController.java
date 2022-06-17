01ad5a0a764fa031cd1383b0c91b07c918c98d77
JSL107 <juneseok0107@naver.com>
2022-06-17 16:14:07 +0900

    Pet Controller create 메소드 생성

---
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/controller/pet/PetController.java
@@ -0,0 +1,28 @@
+package team1.toMyAnimal.controller.pet;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import team1.toMyAnimal.controller.response.Response;
+import team1.toMyAnimal.domain.dto.pet.PetCreateRequest;
+import team1.toMyAnimal.service.pet.PetService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class PetController {
+    private final PetService petService;
+
+    @PostMapping("/api/pets")
+    @ResponseStatus(HttpStatus.CREATED)
+    // @ModelAttribute : 후에 사진 삽입 요청시 @Valid 옆에 
+    public Response create(@Valid PetCreateRequest req){
+        return Response.success(petService.create(req));
+    }
+}
