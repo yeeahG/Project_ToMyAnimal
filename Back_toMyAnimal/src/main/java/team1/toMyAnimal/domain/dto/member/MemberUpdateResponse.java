139a1fb546f597bf97ec1c66560adce8139db744
JSL107 <juneseok0107@naver.com>
2022-06-17 22:25:27 +0900

    회원 정보 수정 응답 구현

--- Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/dto/member/MemberUpdateResponse.java
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/dto/member/MemberUpdateResponse.java
@@ -1,4 +1,10 @@
 package team1.toMyAnimal.domain.dto.member;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class MemberUpdateResponse {
+    private Long id;
 }
