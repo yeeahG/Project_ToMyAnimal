0b21d25d7a7d9ac1092ac2abc44f8ac435f37259
JSL107 <juneseok0107@naver.com>
2022-06-17 21:15:15 +0900

    controller (read, update, delete) method 구현

--- Back_toMyAnimal/src/main/java/team1/toMyAnimal/controller/pet/PetController.java
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/controller/pet/PetController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import team1.toMyAnimal.aop.AssignMemberId;
 import team1.toMyAnimal.controller.response.Response;
 import team1.toMyAnimal.domain.dto.pet.PetCreateRequest;
+import team1.toMyAnimal.domain.dto.pet.PetUpdateRequest;
+import team1.toMyAnimal.domain.dto.post.PostUpdateRequest;
 import team1.toMyAnimal.service.pet.PetService;
 
 import javax.validation.Valid;
@@ -21,7 +23,31 @@ public class PetController {
     @ResponseStatus(HttpStatus.CREATED)
     @AssignMemberId
     // @ModelAttribute : 후에 사진 삽입 요청시 @Valid 옆에
-    public Response create(@Valid @RequestBody PetCreateRequest req){
+    public Response create(@Valid @ModelAttribute PetCreateRequest req) {
         return Response.success(petService.create(req));
     }
+
+    @GetMapping("/api/pets/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Response read(@PathVariable Long id) {
+        return Response.success(petService.read(id));
+    }
+
+    @DeleteMapping("/api/pets/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Response delete(@PathVariable Long id) {
+        petService.delete(id);
+        return Response.success();
+    }
+
+    @PutMapping("/api/pets/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Response update(
+            @PathVariable Long id,
+            @Valid @ModelAttribute PetUpdateRequest req) {
+        return Response.success(petService.update(id, req));
+    }
+            
 }
+
+
