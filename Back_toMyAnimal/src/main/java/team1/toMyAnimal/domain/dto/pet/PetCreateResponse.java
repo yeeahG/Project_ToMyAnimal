b6ab0579c63e9ec882cf64f5706cd2b7c6f3fcc0
JSL107 <juneseok0107@naver.com>
2022-06-17 15:48:45 +0900

    Pet Response 생성

--- Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/dto/pet/PetCreateResponse.java
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/dto/pet/PetCreateResponse.java
@@ -1,4 +1,10 @@
 package team1.toMyAnimal.domain.dto.pet;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class PetCreateResponse {
+    private Long id;
 }
