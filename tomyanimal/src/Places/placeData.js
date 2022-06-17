2935b8dbf13e985db560d2de1262bfc20c50dc71
yeeahG <yeji.sep.k@gmail.com>
2022-06-17 15:09:35 +0900

    Place list 다른지역 추가

--- tomyanimal/src/Places/placeData.js
+++ tomyanimal/src/Places/placeData.js
@@ -2,19 +2,25 @@ export const placeData = [
     {
       type: "훈련소",
       title: "드린겐애견테마파크",
-      addr: "미산동 163-1 KR",
+      addr: "서울 미산동 163-1 KR",
       rating: 4,
     },
     {
       type: "학교",
       title: "반려견 교육 기관 위드",
-      addr: "고봉동 지영로 257-17",
+      addr: "서울 고봉동 지영로 257-17",
       rating: 2,
     },
     {
       type: "동물병원",
       title: "청담우리동물병원",
-      addr: "삼성동 70-6",
+      addr: "서울 삼성동 70-6",
       rating: 3,
+    },
+    {
+      type: "동물병원",
+      title: "24시 범어동물의료센터",
+      addr: "대구 수성구 달구벌대로 2354",
+      rating: 4.43,
     }
   ];
