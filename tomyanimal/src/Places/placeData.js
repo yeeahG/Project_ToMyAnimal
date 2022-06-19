c60facc6722fba1f71313ad83d93c3b639894c68
yeeahG <yeji.sep.k@gmail.com>
2022-06-20 00:04:15 +0900

    Place keyword 구현

--- tomyanimal/src/Places/placeData.js
+++ tomyanimal/src/Places/placeData.js
@@ -1,26 +1,34 @@
 export const placeData = [
     {
+      id: 1,
       type: "훈련소",
       title: "드린겐애견테마파크",
       addr: "서울 미산동 163-1 KR",
+      keyword: "행동교정",
       rating: 4,
     },
     {
+      id: 2,
       type: "학교",
       title: "반려견 교육 기관 위드",
       addr: "서울 고봉동 지영로 257-17",
+      keyword: "행동교정",
       rating: 2,
     },
     {
+      id: 3,
       type: "동물병원",
       title: "청담우리동물병원",
       addr: "서울 삼성동 70-6",
+      keyword: "중성화",
       rating: 3,
     },
     {
+      id: 4,
       type: "동물병원",
       title: "24시 범어동물의료센터",
       addr: "대구 수성구 달구벌대로 2354",
+      keyword: "슬개골",
       rating: 4.43,
     }
   ];
