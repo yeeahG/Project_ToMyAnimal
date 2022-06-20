6dc48678aa5b24afd4f743a97ebd2c83156ee521
JSL107 <juneseok0107@naver.com>
2022-06-21 00:16:07 +0900

    Board Entity 생성

---
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/board/Board.java
@@ -0,0 +1,44 @@
+package team1.toMyAnimal.domain.board;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import team1.toMyAnimal.domain.member.Member;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    @Lob
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    public Board(String title, String content, Member member){
+        this.title = title;
+        this.content = content;
+        this.member = member;
+    }
+
+    public void update(BoardUpdateRequest req) {
+        this.title = req.getTitle();
+        this.content = req.getContent();
+    }
+}
