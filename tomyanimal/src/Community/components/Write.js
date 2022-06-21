dd78697f45a24b1903a64eceab0cd4d13e12c8a3
yeeahG <yeji.sep.k@gmail.com>
2022-06-21 18:19:10 +0900

    Board write 기능

--- tomyanimal/src/Community/components/Write.js
+++ tomyanimal/src/Community/components/Write.js
@@ -1,24 +1,64 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 const Write = ( {openButton} ) => {
     const [isOpen, setOpen] = useState(true);
+    const [post, setPost] = useState([]);
+    const [newTitle, setNewTitle] = useState("");
+    const [newContent, setNewContent] = useState("");
+    const [error, setError] = useState("");
+
+
+    const addPost = async () => {
+        const newPost = {
+            title: newTitle,
+            body: newContent,
+            userId: "yeji"
+        }
+
+        if(newTitle != "" || newContent != "") {
+            await axios.post('https://jsonplaceholder.typicode.com/posts', post)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('성공:', data);
+            })
+  
+            .catch((error) => {
+                console.error('실패:', error);
+            });
+            setPost(newPost);
+            console.log(post);
+            alert('작성이 완료되었습니다')
+        } else {
+            setError("한 글자 이상 입력하세요")
+        }
+    }
 
   return (
     <div className='write__box'>
+        {error}
         <div className='write__head'>
-            <input placeholder='Write title' type="text"/>
+            <input 
+                placeholder='Write title' 
+                type="text"
+                name="title"
+                onChange={(e) => {setNewTitle(e.target.value)}}
+            />
         </div>
 
         <div className='write__container'>
-            <textarea className='board__write' />
+            <textarea 
+                className='board__write'
+                onChange={(e) => {setNewContent(e.target.value)}}
+            />
         </div>
 
+
+        <button onClick={addPost}>
+            Add
+        </button>
         <button onClick={openButton}>
             {isOpen ? "Close" : ""}
-        </button>
-
-        <button onClick={openButton}>
-            Add
         </button>
 
     </div>
