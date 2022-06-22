27ab338aa7ddc3cff30374e33500d2ea7e8ea0e6
JSL107 <juneseok0107@naver.com>
2022-06-22 22:51:23 +0900

    검색시 조건 PetReadCondition

---
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/dto/pet/PetReadCondition.java
@@ -0,0 +1,15 @@
+package team1.toMyAnimal.domain.dto.pet;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Null;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PetReadCondition {
+    @Null
+    private Long memberId;
+}
