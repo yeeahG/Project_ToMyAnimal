f108bc870d5b80ba7aed69856d12d026e6928128
JSL107 <juneseok0107@naver.com>
2022-06-22 16:32:56 +0900

    예약 테이블 생성

---
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/reservation/Reservation.java
@@ -0,0 +1,47 @@
+package team1.toMyAnimal.domain.reservation;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import team1.toMyAnimal.domain.member.Member;
+import team1.toMyAnimal.domain.pet.Pet;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Reservation {
+
+    // 날짜, 장소, 시간, 동물의 종류, 회원 ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Long id;
+    private LocalDate date;
+    private LocalDateTime time;
+    private String type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Pet pet;
+
+    public Reservation(LocalDate date, LocalDateTime time, String type, Member member, Pet pet){
+        this.date = date;
+        this.time = time;
+        this.type = type;
+        this.member = member;
+        this.pet = pet;
+    }
+
+}
