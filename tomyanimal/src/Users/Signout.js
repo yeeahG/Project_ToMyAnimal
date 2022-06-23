84d8c02c8728cf46dfa5ad3a8dda6a7ffcf77d15
yeeahG <yeji.sep.k@gmail.com>
2022-06-23 17:25:35 +0900

    User signout 기능 수정

--- tomyanimal/src/Users/Signout.js
+++ tomyanimal/src/Users/Signout.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
  
 const Signout = () => {
+  const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const [isOpen, setOpen] = useState(false);
   const userid = localStorage.getItem('userid')
+
+  useEffect(() => {
+    axios({
+      method: 'get', 
+      url: 'http://localhost:8084/api/members/' + userid,
+    }).then((user) => {
+      setPassword(user.data.result.data['userPhoneNumber']) 
+    })
+  }, []);
 
   const userDelete = async () => {
     axios.delete(`http://localhost:8084/api/members/${userid}`, {
@@ -20,6 +34,19 @@ const Signout = () => {
     });
   }
   
+  //console.log(checkPassword);
+
+  const checkHandler = (e) => {
+    e.preventDefault();
+    console.log('contacn check');
+
+    if (password === checkPassword) {
+      setOpen(true)
+    } else {
+      setError('연락처를 다시 입력하세요')
+    }
+  }
+
   return (
     <div>
       <div className='userinfo__subtitle'>
@@ -27,11 +54,11 @@ const Signout = () => {
       </div>
       
       <p>탈퇴하기</p>
-      <p>비밀번호 재 확인 후 확인창 띄우고 삭제</p>
-
+      <p>비밀번호 재 확인(전화번호로 임시대체) 후 확인창 띄우고 삭제</p>
+      <p>input.password === DB의 userid password ? 탈퇴창 : error</p>
       <div className='userinfo__content'>
-        input.password === DB의 userid password ? 탈퇴창 : error
-        <form>
+        {error}
+        <form onSubmit={checkHandler}>
           <thead>
             <tr>
               <th>id</th>
@@ -39,13 +66,28 @@ const Signout = () => {
               <th>Contact</th>
             </tr>
           </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  placeholder='연락처를 입력하세요' 
+                  onChange={(e) => setCheckPassword(e.target.value)}
+                />
+              </td>
+            </tr>
+          </tbody>
+          <input type="submit" className='welcome__btn' value="SEND" />
         </form>
 
+        {isOpen ?
         <div className='welcome'>
           <button className='welcome__btn' onClick={userDelete}>
             <a href="/">Delete</a>
           </button>
         </div>
+        :
+        ""
+        }
       </div>
     </div>
   )
