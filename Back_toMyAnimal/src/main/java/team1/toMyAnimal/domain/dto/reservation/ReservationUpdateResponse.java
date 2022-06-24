af1183ced71acc57edda31cc974b316f8cd2225b
juneseok0107@naver.com <juneseok0107@naver.com>
2022-06-24 16:43:31 +0900

    수정 요청 및 응답요청 ReservationUpdateRequest & ReservationUpdateResponse 구현

---
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/dto/reservation/ReservationUpdateResponse.java
@@ -0,0 +1,10 @@
+package team1.toMyAnimal.domain.dto.reservation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ReservationUpdateResponse {
+    private Long id;
+}
