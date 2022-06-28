fb61271a676bc1c7af103ced946fc598cc152d75
yeeahG <yeji.sep.k@gmail.com>
2022-06-28 18:11:30 +0900

    Animal info 사진 전송

--- tomyanimal/src/Users/components/AnimalAdd.js
+++ tomyanimal/src/Users/components/AnimalAdd.js
@@ -33,37 +33,12 @@ const AnimalAdd = () => {
         formData.set('birthday', animal.birthday)
         formData.set('weight', animal.weight)
 
-        //const photoFile = document.getElementById('photo')
-        //formData.append("addedImages", photoFile.files[0]);
+        //const file = document.getElementById('photo')
+        //formData.append("addedImages", file.files[0]);
+
+        Object.values(imgFile).forEach((file) => formData.append("images", file));
 
         if(animalName!=="" && animalId!=="" && animalAge!=="" && animalWeight!=="" ) {
-
-            // await fetch('https://jsonplaceholder.typicode.com/posts', {
-            /*
-            await fetch('http://localhost:8084/api/pets', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    'Authorization': localStorage.getItem('logintoken'),
-                },
-                //body: JSON.stringify(animal),
-                body: formData,
-            })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log('성공:', data);
-            })
-
-            .catch((error) => {
-                console.error('실패:', error);
-            });
-
-            localStorage.setItem("animalinfo", JSON.stringify(animal))
-            console.log(formData);
-
-            alert('가입이 완료되었습니다')
-            navigate('/user')
-            */
             axios.post('http://localhost:8084/api/pets', formData, {
                headers: {
                 'Content-Type': 'multipart/form-data',
@@ -89,13 +64,34 @@ const AnimalAdd = () => {
         }
     }
 
+    const [imgBase64, setImgBase64] = useState([]); // 파일 미리보기
+    const [imgFile, setImgFile] = useState(null);	//파일	
+
     //image upload
     const onChangeImg  = (e) => {
+        console.log(e.target.files);
         e.preventDefault();
+        setImgFile(e.target.files)
+        setImgBase64([])
 
-        if(e.target.files){
-            const uploadFile = e.target.files[0]
-            console.log(uploadFile)
+        for(let i=0; i<e.target.files.length; i++) {
+        if(e.target.files[i]){
+            //const uploadFile = e.target.files[0]
+            //console.log(uploadFile)
+            let reader = new FileReader();
+            reader.readAsDataURL(e.target.files[i])
+
+            reader.onloadend = () => {
+                const base64 = reader.result;
+                console.log(base64);
+
+                if(base64) {
+                    var base64Sub = base64.toString()
+
+                    setImgBase64(imgBase64 => [...imgBase64, base64Sub]);
+                }
+            }
+        }
         }
     }
 
@@ -118,13 +114,21 @@ const AnimalAdd = () => {
                 <tbody>
                     <tr>
                         {/*<td><input label="사진" name="photo" id="photo" placeholder="사진" type='file' onChange={(e) => setAnimalPhoto(e.target.files)} /></td>*/}
-                        <td><input label="사진" name="photo" id="photo" placeholder="사진" type='file' onChange={onChangeImg} /></td>
+                        <td><input label="사진" name="photo" id="file" placeholder="사진" type='file' onChange={onChangeImg} multiple="multiple"/></td>
                         <td><input label="이름" name="animalname" placeholder="이름" required onChange={(e) => setAnimalName(e.target.value)} /></td>
                         <td><input label="등록번호" name="animalId" placeholder="등록번호" required onChange={(e) => setAnimalId(e.target.value)} /></td>
                         <td><input label="생일" name="age" placeholder="나이" type='date' required onChange={(e) => setAnimalAge(e.target.value)}/></td>
                         <td><input label="무게" name="kg" placeholder="무게" required onChange={(e) => setAnimalWeight(e.target.value)} /></td>
                     </tr>
+                {imgBase64.map((item) => {
+                    <img 
+                    src={item}
+                    alt="First file"
+                    style={{width:"100%",height:"550px"}}
+                    />
+                })}
                 </tbody>
+
             </table>
         </div>
 
