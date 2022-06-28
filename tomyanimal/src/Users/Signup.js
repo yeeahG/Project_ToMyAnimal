7831fdbd7fe2c252c19996cb169166f2295ddc8f
yeeahG <yeji.sep.k@gmail.com>
2022-06-28 23:13:58 +0900

    User login email 추가

--- tomyanimal/src/Users/Signup.js
+++ tomyanimal/src/Users/Signup.js
@@ -6,8 +6,9 @@ const Signup = () => {
     let [phoneNumber, setPhoneNumber] = useState("");
     let [username, setUsername] = useState("");
     let [signinId, setSigninId] = useState("");
+    let [signinEmail, setSigninEmail] = useState("");
     let [signinPassword, setSigninPassword] = useState("");
-    let [signinPasswordCheck, setSigninPasswordCheck] = useState("");
+    //let [signinPasswordCheck, setSigninPasswordCheck] = useState("");
     
     let [savedPhoneNumber, setSavedPhoneNumber] = useState("");
     let [savedUsername, setSavedUsername] = useState("");
@@ -24,11 +25,12 @@ const Signup = () => {
           userPhoneNumber: phoneNumber,
           userId: signinId,
           username: username,
+          email: signinEmail,
           password: signinPassword,
         }
         console.log(item);
     
-        if( phoneNumber!="" || signinId!="" || username!="" || signinPassword!="" || signinPasswordCheck!="") {
+        if( phoneNumber!="" || signinId!="" || username!="" || signinPassword!="") {
           // if (signinPassword === signinPasswordCheck) {
     
             await fetch('http://localhost:8084/api/signup', {
@@ -77,6 +79,7 @@ const Signup = () => {
         <input label="연락처" name="phone" placeholder="연락처" required onChange={(e) => {setPhoneNumber(e.target.value)}}></input>
         <input label="이름" name="username" placeholder="이름" required onChange={(e) => {setUsername(e.target.value)}}></input>
         <input label="아이디" name="userId" placeholder="아이디" required onChange={ (e) => {setSigninId(e.target.value)}}></input>
+        <input label="이메일" name="userEmail" placeholder="이메일" required onChange={ (e) => {setSigninEmail(e.target.value)}}></input>
         <input label="비밀번호" name="password" placeholder="비밀번호" type="password" required onChange={(e) => {setSigninPassword(e.target.value)}}></input>
         {/* <input label="비밀번호 확인" name="passwordConfirm" placeholder="비밀번호 확인" type="password" required onChange={(e) => {setSigninPasswordCheck(e.target.value)}}></input>  */}
                 
