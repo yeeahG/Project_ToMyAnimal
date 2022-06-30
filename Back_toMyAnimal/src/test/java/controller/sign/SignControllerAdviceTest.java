d5d90c5045d63f0672c6cf69481c53d42ac1ce26
JSL107 <juneseok0107@naver.com>
2022-06-30 10:47:42 +0900

    feedback 내용 수정 SignControllerAdviceTest 작성

---
+++ Back_toMyAnimal/src/test/java/controller/sign/SignControllerAdviceTest.java
@@ -0,0 +1,120 @@
+package controller.sign;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import team1.toMyAnimal.controller.sign.SignController;
+import team1.toMyAnimal.domain.dto.sign.SignInRequest;
+import team1.toMyAnimal.domain.dto.sign.SignUpRequest;
+import team1.toMyAnimal.exception.*;
+import team1.toMyAnimal.service.sign.SignService;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class SignControllerAdviceTest {
+    @InjectMocks
+    SignController signController;
+    @Mock
+    SignService signService;
+    MockMvc mockMvc;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void beforeEach() {
+        mockMvc = MockMvcBuilders.standaloneSetup(signController).setControllerAdvice(new ExceptionAdvice()).build();
+    }
+
+    @Test
+    void signInLoginFailureExceptionTest() throws Exception {
+        // given
+        SignInRequest req = new SignInRequest("id1", "123456a!");
+        given(signService.signIn(any())).willThrow(LoginFailureException.class);
+
+        // when, then
+        mockMvc.perform(
+                        post("/api/signin")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(req)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void signInMethodArgumentNotValidExceptionTest() throws Exception {
+        // given
+        SignInRequest req = new SignInRequest("id1", "1234567");
+
+        // when, then
+        mockMvc.perform(
+                        post("/api/signin")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(req)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void signUpMemberEmailAlreadyExistsExceptionTest() throws Exception {
+        // given
+        SignUpRequest req = new SignUpRequest("id1","01011112222", "name", "123456a!", "email@email.com");
+        doThrow(MemberIdAlreadyExistsException.class).when(signService).signUp(any());
+
+        // when, then
+        mockMvc.perform(
+                        post("/api/signup")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(req)))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    void signUpMemberNicknameAlreadyExistsExceptionTest() throws Exception {
+        // given
+        SignUpRequest req = new SignUpRequest("id1","01011112222", "name", "123456a!", "email@email.com");
+        doThrow(MemberPhoneNumberAlreadyExistsException.class).when(signService).signUp(any());
+
+        // when, then
+        mockMvc.perform(
+                        post("/api/signup")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(req)))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    void signUpRoleNotFoundExceptionTest() throws Exception {
+        // given
+        SignUpRequest req = new SignUpRequest("id1","01011112222", "name", "123456a!", "email@email.com");
+        doThrow(RoleNotFoundException.class).when(signService).signUp(any());
+
+        // when, then
+        mockMvc.perform(
+                        post("/api/signup")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(req)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void signUpMethodArgumentNotValidExceptionTest() throws Exception {
+        // given
+        SignUpRequest req = new SignUpRequest("", "", "", "","");
+
+        // when, then
+        mockMvc.perform(
+                        post("/api/signup")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(req)))
+                .andExpect(status().isBadRequest());
+    }
+}
