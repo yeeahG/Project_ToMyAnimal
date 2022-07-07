871755307cb06b167c7fc057a30e24c591ecefaa
JSL107 <juneseok0107@naver.com>
2022-07-08 00:08:13 +0900

    예약(Reservation) 종류 POSTMAN에서 입력되지 않았던 오류 해결

--- Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/dto/reservation/ReservationUpdateRequest.java
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/dto/reservation/ReservationUpdateRequest.java
@@ -1,13 +1,16 @@
 package team1.toMyAnimal.domain.dto.reservation;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import team1.toMyAnimal.domain.reservation.ReservationType;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -15,7 +18,8 @@ import java.time.LocalDateTime;
 public class ReservationUpdateRequest {
 
     @NotBlank(message = "변경하실 날짜를 입력해주세요.")
-    private LocalDate date;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
 
     @NotBlank(message = "예약의 종류를 입력해주세요")
     private ReservationType type;
--- Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/reservation/ReservationType.java
+++ Back_toMyAnimal/src/main/java/team1/toMyAnimal/domain/reservation/ReservationType.java
@@ -1,8 +1,14 @@
 package team1.toMyAnimal.domain.reservation;
 
 public enum ReservationType {
+//    SURGERY, // 수술
+//    INOCULATION, // 접종
+//    DIAGNOSIS, // 진료
+//    INQUIRY // 문의
+
     수술,
     접종,
     진료,
     문의
+
 }
