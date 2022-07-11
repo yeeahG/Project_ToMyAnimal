743c4c253e7414cbaa88ca2c05e1c241da0b623b
yeeahG <yeji.sep.k@gmail.com>
2022-07-11 23:20:46 +0900

    Reservation 기능 수정

--- tomyanimal/src/Users/Reservation/UserReservation.js
+++ tomyanimal/src/Users/Reservation/UserReservation.js
@@ -6,9 +6,11 @@ const UserReservation = () => {
   const [reservDate, setReservDate] = useState("");
   const [reservType, setReservType] = useState("");
   const [reservAnimal, setReservAnimal] = useState("");
+  const [reservData, setReservData] = useState("");
 
   const loginId = localStorage.getItem('userid');
 
+  //const myreservlist = [];
 
   useEffect(() => {
     axios({
@@ -21,6 +23,11 @@ const UserReservation = () => {
       setReservDate(reservation.data.result.data[0].date) 
       setReservType(reservation.data.result.data[0].type)
       setReservAnimal(reservation.data.result.data[0].animal.name)
+
+      //예약 내역이 여러개가 들어올때를 대비하여
+      // for (let i=0; i<reservation.data.result.data.length; i++) {
+      //   myreservlist.push(reservation.data.result.data[i])
+      // } setReservData(myreservlist)
     })
   }, []);
 
@@ -43,12 +50,24 @@ const UserReservation = () => {
         <div></div>
 
         <div className='myreserv__content'>
-          <div className='myreserv__data'>
+          
+           <div className='myreserv__data'>
             <h3 className='myreserv__date'>{reservDate.slice(5,10)}</h3>
             <p>{reservAnimal}</p>
             <p>{reservDate.slice(11, 16)}</p>
             <p>{reservType}</p>
+          </div> 
+
+          {/*예약 내역이 여러개가 들어올때를 대비하여
+          {reservData.map((it) =>
+          <div className='myreserv__data'>
+            <h3 className='myreserv__date'>{it.date.slice(5,10)}</h3>
+            <p>{it.animal.name}</p>
+            <p>{it.date.slice(11, 16)}</p>
+            <p>{it.type}</p>
           </div>
+          )}*/}
+
         </div>
 
       </section>
