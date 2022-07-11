4f44193a04a815b2b5a7d16751dcc9704d1a8665
KimEunhyuk <bang1676@naver.com>
2022-07-12 02:49:04 +0900

    동물병원 크롤링 최종코드

--- Data_toMyAnimal/동물병원 크롤링.py
+++ Data_toMyAnimal/동물병원 크롤링.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[3]:
 
 
 def animalhospital(place):
@@ -39,134 +39,6 @@ def animalhospital(place):
     
     target = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/div[1]/div[7]')
     target_searchplace = driver.find_element(By.ID, 'info.search.place')
-    # pagebutton을 target.find_element 했더니 알수없는
-
-    zeroless16 = list(range(16))
-    zeroless16.remove(0)
-
-    name_list = []
-    star_list = []
-    reviewtotal_list = []
-    addr_list = []
-    tel_list = []
-
-
-    page = range(50)
-    for ultag in target_searchplace.find_elements(By.XPATH, "./ul[1]"):
-        if ultag.text != "":
-            for k in page:
-                for j in range(5):
-                    pagebutton_12345 = pagebutton.find_element(By.XPATH, "./div[1]/a[{}]".format(j+1))
-                    pagebutton_12345.click()
-                    time.sleep(1)
-                    for i in zeroless16:
-
-                        print("--------------------------")
-                        print(i)
-
-                        subli = ultag.find_element(By.XPATH, "./li[{}]".format(i))
-                        try :
-                            name = subli.find_element(By.XPATH, "./div[3]/strong/a[2]")
-                            star = subli.find_element(By.XPATH, "./div[4]/span[1]/em")
-                            reviewtotal = subli.find_element(By.XPATH, "./div[4]/span[1]/a")
-                            addr = subli.find_element(By.XPATH, "./div[5]/div[2]")
-                            tel = subli.find_element(By.XPATH, "./div[5]/div[4]/span[1]")
-
-                            print(name.text, "= name")
-                            print(star.text, "= star")
-                            print(reviewtotal.text, "= reviewtotal")
-                            print(addr.text, "= addr")
-                            print(tel.text, "= tel")
-
-                            name_list.append(name.text)
-                            star_list.append(star.text)
-                            reviewtotal_list.append(reviewtotal.text)
-                            addr_list.append(addr.text)
-                            tel_list.append(tel.text)
-                        except:
-                            print("광고")
-
-                    if j == 4 :
-                        pagebutton_next = pagebutton.find_element(By.XPATH, "./div[1]/button[2]")
-                        pagebutton_next.click()
-                        time.sleep(1)
-                        
-                        
-    animalhospital_df = pd.DataFrame({ '병원이름':name_list, '별점':star_list, '리뷰수':reviewtotal_list, '주소':addr_list, '전화':tel_list })
-    animalhospital_df['주소'] = animalhospital_df['주소'].apply(lambda x: tuple(x.split("\n")))
-    animalhospital_df_dropdupl = animalhospital_df.copy()
-    animalhospital_df_dropdupl.drop_duplicates(['병원이름', '주소'], keep = 'first')
-    animalhospital_df_dropdupl.to_csv('animalhospital_'+place+'.csv')
-    
-    
-    
-    
-    # 광고 걸러내면서 크롤링 되고 페이지가 넘어가지 않을때 에러나면서 csv파일이 안만들어지는 코드
-
-
-# In[21]:
-
-
-animalhospital("대전")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[22]:
-
-
-def animalhospital(place):
-    
-    from selenium.webdriver.common.keys import Keys
-    from selenium import webdriver
-    import time
-    from selenium.webdriver.common.by import By
-    import pandas as pd
-
-    driver = webdriver.Chrome("D:/20220103_lab/14.python/data/chrome/103/chromedriver")
-    driver.get("https://map.kakao.com/")
-    time.sleep(3)
-
-    dimmedLayer = driver.find_element_by_css_selector("div#dimmedLayer.DimmedLayer")
-    dimmedLayer.click()
-    time.sleep(1)
-
-    search_input_tag = driver.find_element(By.ID, 'search.keyword.query')
-    search_input_tag.send_keys('{} 동물병원'.format(place))
-    time.sleep(1)
-
-    search_button_tag = driver.find_element(By.ID, 'search.keyword.submit')
-    search_button_tag.click()
-    time.sleep(2)
-
-    search_place_more = driver.find_element(By.ID, 'info.search.place.more')
-    search_place_more.click()
-    time.sleep(1)
-
-    pagebutton = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/div[1]/div[7]/div[6]')
-    pagebutton_1 = pagebutton.find_element(By.XPATH, "./div[1]/a[1]")
-    pagebutton_1.click()
-    time.sleep(1) 
-    
-    target = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/div[1]/div[7]')
-    target_searchplace = driver.find_element(By.ID, 'info.search.place')
-    # pagebutton을 target.find_element 했더니 알수없는
 
     zeroless16 = list(range(16))
     zeroless16.remove(0)
@@ -188,10 +60,6 @@ def animalhospital(place):
                         pagebutton_12345.click()
                         time.sleep(1)
                         for i in zeroless16:
-
-                            print("--------------------------")
-                            print(i)
-
                             subli = ultag.find_element(By.XPATH, "./li[{}]".format(i))
                             try :
                                 name = subli.find_element(By.XPATH, "./div[3]/strong/a[2]")
@@ -200,19 +68,13 @@ def animalhospital(place):
                                 addr = subli.find_element(By.XPATH, "./div[5]/div[2]")
                                 tel = subli.find_element(By.XPATH, "./div[5]/div[4]/span[1]")
 
-                                print(name.text, "= name")
-                                print(star.text, "= star")
-                                print(reviewtotal.text, "= reviewtotal")
-                                print(addr.text, "= addr")
-                                print(tel.text, "= tel")
-
                                 name_list.append(name.text)
                                 star_list.append(star.text)
                                 reviewtotal_list.append(reviewtotal.text)
                                 addr_list.append(addr.text)
                                 tel_list.append(tel.text)
                             except:
-                                print("광고")
+                                print("광고 필터링")
 
                         if j == 4 :
                             pagebutton_next = pagebutton.find_element(By.XPATH, "./div[1]/button[2]")
@@ -225,10 +87,17 @@ def animalhospital(place):
         animalhospital_df_dropdupl = animalhospital_df.copy()
         animalhospital_df_dropdupl.drop_duplicates(['병원이름', '주소'], keep = 'first')
         animalhospital_df_dropdupl.to_csv('animalhospital_'+place+'.csv')
+        print("크롤링 완료")
 
 
-# In[23]:
+# In[8]:
 
 
-animalhospital("대전")
+animalhospital("서울")
+
+
+# In[ ]:
+
+
+
 
