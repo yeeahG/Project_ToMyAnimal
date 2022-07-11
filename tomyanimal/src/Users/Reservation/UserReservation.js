c3440db13c935064ed7ad9d2571be068b5b4678a
yeeahG <yeji.sep.k@gmail.com>
2022-07-11 23:13:04 +0900

    Reservation 부분 UIUX 수정

--- tomyanimal/src/Users/Reservation/UserReservation.js
+++ tomyanimal/src/Users/Reservation/UserReservation.js
@@ -3,50 +3,55 @@ import axios from 'axios';
 import './UserReservation.css'
 
 const UserReservation = () => {
-    const [reservDate, setReservDate] = useState("");
-    const [reservType, setReservType] = useState("");
-    const [reservAnimal, setReservAnimal] = useState("");
+  const [reservDate, setReservDate] = useState("");
+  const [reservType, setReservType] = useState("");
+  const [reservAnimal, setReservAnimal] = useState("");
 
-    const loginId = localStorage.getItem('userid');
+  const loginId = localStorage.getItem('userid');
 
 
-    useEffect(() => {
-        axios({
-          method: 'get', 
-          url: process.env.REACT_APP_BACK_BASE_URL + `api/my-reservation?memberId=${loginId}`,
-          headers: {
-            Authorization: localStorage.getItem('logintoken') 
-          }
-        }).then((reservation) => {
-            setReservDate(reservation.data.result.data[0].date) 
-            setReservType(reservation.data.result.data[0].type)
-            setReservAnimal(reservation.data.result.data[0].animal.name)
-        })
-      }, []);
+  useEffect(() => {
+    axios({
+      method: 'get', 
+      url: process.env.REACT_APP_BACK_BASE_URL + `api/my-reservation?memberId=${loginId}`,
+      headers: {
+        Authorization: localStorage.getItem('logintoken') 
+      }
+    }).then((reservation) => {
+      setReservDate(reservation.data.result.data[0].date) 
+      setReservType(reservation.data.result.data[0].type)
+      setReservAnimal(reservation.data.result.data[0].animal.name)
+    })
+  }, []);
 
   return (
-    <div>
+    <div className='myreserv__main__container'>
 
-        <div className='userinfo__subtitle'>
-            <h1>Reservation</h1>
+      <div className='myreserv__subtitle'>
+
+        <h3>Reservation<br/>
+        List</h3>
+        <div className='myreserv__background'>
+          {/* <img  src={`${process.env.PUBLIC_URL}/public_assets/보건증1.png`}/> */}
+        </div>
+        
+      </div>
+
+
+      <section className='myreserv__container'>
+
+        <div></div>
+
+        <div className='myreserv__content'>
+          <div className='myreserv__data'>
+            <h3 className='myreserv__date'>{reservDate.slice(5,10)}</h3>
+            <p>{reservAnimal}</p>
+            <p>{reservDate.slice(11, 16)}</p>
+            <p>{reservType}</p>
+          </div>
         </div>
 
-        <section className='myreserv__content'>
-
-            <div>
-                <img  src={`${process.env.PUBLIC_URL}/public_assets/보건증1.png`}/>
-            </div>
-
-            <div>
-                <h3>예약일</h3>
-                <p>날짜 {reservDate.slice(0,10)} 시간 {reservDate.slice(11, 16)}</p>
-                <h3>예약내역</h3>
-                <p>{reservType}</p>
-                <h3>해당동물</h3>
-                <p>{reservAnimal}</p>
-            </div>
-
-        </section>
+      </section>
         
     </div>
   )
