9847dd013ae826019c44c5c425edcf1c7f9b8b0e
yeeahG <yeji.sep.k@gmail.com>
2022-07-21 17:56:07 +0900

    Reservation 수정 기능

--- tomyanimal/src/Users/Reservation/UserReservation.js
+++ tomyanimal/src/Users/Reservation/UserReservation.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import './UserReservation.css'
 import ReservationList from './ReservationList';
 import { authInstance } from '../../utils/api';
@@ -65,13 +64,6 @@ const UserReservation = () => {
 
             { (reservData.success === true) ?
               <>
-                {/* <div>
-                  <h3 className='myreserv__date'>{reservDate.slice(5,10)}</h3>
-                  <p>{reservAnimal}</p>
-                  <p>{reservDate.slice(11, 16)}</p>
-                  <p>{reservType}</p>
-                </div> */}
-
                 {reservList.map((it) => (
                   <ReservationList 
                     key={it.id}
@@ -84,16 +76,6 @@ const UserReservation = () => {
             <h3>예약 내역이 없습니다</h3>
             }
           </div> 
-
-          {/*예약 내역이 여러개가 들어올때를 대비하여
-          {reservData.map((it) =>
-          <div className='myreserv__data'>
-            <h3 className='myreserv__date'>{it.date.slice(5,10)}</h3>
-            <p>{it.animal.name}</p>
-            <p>{it.date.slice(11, 16)}</p>
-            <p>{it.type}</p>
-          </div>
-          )}*/}
 
         </div>
 
